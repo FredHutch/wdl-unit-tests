5ddbb5efd17ecdd0a8adfbc6b6d19c56b8618624
Scott Chamberlain <sachamber@fredhutch.org>
2025-01-03 14:38:36 -0800

    move logic from womtool validate gh action yml to python pytest setup; needs additional pkg sh

---
+++ tests/validate/test-validate.py
@@ -0,0 +1,12 @@
+from utils import Womtool
+
+should_fail = ["badFile"]
+
+wom = Womtool()
+
+def test_womtool_validate(wdl_path):
+    out = wom.validate(wdl_path)
+    if wdl_path in should_fail:
+        assert not out
+    else:
+        assert out
---
+++ tests/validate/utils.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+
+import sh
+
+class Womtool(object):
+    """Womtool class"""
+    def __init__(self):
+        self.womtool_path = os.getenv("WOMTOOL_PATH")
+        if not self.womtool_path:
+            raise Exception("failed setting WOMTOOL_PATH")
+        self.womtool = sh.Command("java").bake("-jar", self.womtool_path)
+
+    def has_inputs(self, wdl_path):
+        path = Path(f"{wdl_path}/inputs.json")
+        if path.exists():
+            self.womtool = self.womtool.bake(i=str(path))
+
+    def validate(self, wdl_path):
+        try:
+            self.has_inputs(wdl_path)
+            self.womtool(
+                "validate",
+                f"{wdl_path}/{wdl_path}.wdl",
+            )
+        except sh.ErrorReturnCode as e:
+            print(f"Command {e.full_cmd} exited with {e.exit_code}")
+            return False
+
+        return True
