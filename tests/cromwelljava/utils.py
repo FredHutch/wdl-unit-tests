eaa654b509c258b45d4fad21aea8cabbbdbd9577
Scott Chamberlain <sachamber@fredhutch.org>
2025-01-09 14:14:51 -0800

    added test-run test for cromwell java run tests; added CromwellJava class for shelling out to java cromwell jar; updated gh actoins files

--- tests/cromwelljava/utils.py
+++ tests/cromwelljava/utils.py
@@ -18,16 +18,52 @@ class Womtool(object):
         if path.exists():
             self.womtool = self.womtool.bake(i=str(path))
 
-    def validate(self, wdl_path, verbose=False):
+    def validate(self, wdl_path, show_cmd=False):
         try:
-            self.has_inputs(wdl_path)
             self.womtool = self.womtool.bake(
                 "validate",
                 f"{wdl_path}/{wdl_path}.wdl",
             )
-            if verbose:
+            self.has_inputs(wdl_path)
+            if show_cmd:
                 print(self.womtool)
             self.womtool()
+        except sh.ErrorReturnCode as e:
+            print(f"Command {e.full_cmd} exited with {e.exit_code}")
+            return False
+
+        return True
+
+class CromwellJava(object):
+    """Cromwell Java class - run Cromwell Java jar"""
+
+    def __init__(self):
+        self.cromwell_path = os.getenv("CROMWELL_PATH")
+        if not self.cromwell_path:
+            raise Exception("failed setting CROMWELL_PATH")
+        self.cromwell = sh.Command("java").bake("-jar", self.cromwell_path)
+
+    def has_inputs(self, wdl_path):
+        path = Path(f"{wdl_path}/inputs.json")
+        if path.exists():
+            self.cromwell = self.cromwell.bake(i=str(path))
+
+    def has_options(self, wdl_path):
+        path = Path(f"{wdl_path}/options.json")
+        if path.exists():
+            self.cromwell = self.cromwell.bake(o=str(path))
+
+    def run(self, wdl_path, show_cmd=False):
+        try:
+            self.cromwell = self.cromwell.bake(
+                "run",
+                f"{wdl_path}/{wdl_path}.wdl",
+            )
+            self.has_inputs(wdl_path)
+            self.has_options(wdl_path)
+            if show_cmd:
+                print(self.cromwell)
+            self.cromwell()
         except sh.ErrorReturnCode as e:
             print(f"Command {e.full_cmd} exited with {e.exit_code}")
             return False
