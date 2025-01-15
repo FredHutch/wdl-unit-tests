3e35b9217b00305fe935464563d48cf28494c0b6
Scott Chamberlain <sachamber@fredhutch.org>
2025-01-15 09:32:34 -0800

    fix file path

--- tests/cromwellapi/test-failures.py
+++ tests/cromwellapi/test-failures.py
@@ -4,7 +4,7 @@
 def test_failures_initial_state(cromwell_api, submit_wdls):
     """Checking for failures works for initial state"""
     params = {"includeKey": "failures", "includeKey": "jobId"}
-    fail = list(filter(lambda x: "parseBatchFile" in x["path"], submit_wdls))
+    fail = list(filter(lambda x: "badRunParseBatchFile" in x["path"], submit_wdls))
     res = cromwell_api.metadata(fail[0]["id"], params=params)
     assert isinstance(res, dict)
     assert res == {}
@@ -13,7 +13,7 @@ def test_failures_initial_state(cromwell_api, submit_wdls):
 # def test_failures_final_state(cromwell_api, submit_wdls):
 #     """Checking for failures works for final state"""
 #     params = {"includeKey": "failures", "includeKey": "jobId"}
-#     fail = list(filter(lambda x: "parseBatchFile" in x["path"], submit_wdls))
+#     fail = list(filter(lambda x: "badRunParseBatchFile" in x["path"], submit_wdls))
 #     print(f"fail0: {fail[0]}")
 #     res = cromwell_api.metadata(fail[0]["id"], params=params)
 #     print(f"cromwell_api.metadata output:{res}")
