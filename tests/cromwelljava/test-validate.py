7c41521ff52adc2ac40418493ac01d4c751739a8
Scott Chamberlain <sachamber@fredhutch.org>
2025-01-15 09:18:13 -0800

    rename badFile to badVal prefix; adjust prefix check in womtool and cromwell run python test files

--- tests/cromwelljava/test-validate.py
+++ tests/cromwelljava/test-validate.py
@@ -5,7 +5,7 @@ wom = Womtool()
 
 def test_womtool_validate(wdl_path):
     is_valid = wom.validate(wdl_path)
-    if wdl_path.startswith("bad"):
+    if wdl_path.startswith("badVal"):
         assert not is_valid
     else:
         assert is_valid
