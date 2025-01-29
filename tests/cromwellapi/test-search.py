fa96761038a2283a9db064fd670831de70463e07
Scott Chamberlain <sachamber@fredhutch.org>
2025-01-29 09:04:14 -0800

    for cromwell search dont match against query b/c dates change

--- tests/cromwellapi/test-search.py
+++ tests/cromwellapi/test-search.py
@@ -17,7 +17,15 @@ def contains_sublist(list1, list2):
     return set(list2).issubset(set(list1))
 
 
-@pytest.mark.vcr
+# We need to not match on "query" here because CromwellApi.search
+# changes the date depending on what day the query is run, leading
+# to no match against the stored cassette
+VCR_SEARCH_CONFIG = {
+    "match_on": ["method", "scheme", "path"],
+}
+
+
+@pytest.mark.vcr(**VCR_SEARCH_CONFIG)
 def test_search_no_results(cromwell_api):
     """Checking that search works with no results"""
     out = cromwell_api.search(days=-2)
@@ -27,7 +35,7 @@ def test_search_no_results(cromwell_api):
     assert out["results"] == []
 
 
-@pytest.mark.vcr
+@pytest.mark.vcr(**VCR_SEARCH_CONFIG)
 def test_search_results(cromwell_api):
     """Checking that search works when there MIGHT be results"""
     out = cromwell_api.search(days=1)
