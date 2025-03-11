9554d33f082c487b5358939b21a0ac8c483e1466
tefirman <tefirman@gmail.com>
2025-03-11 13:25:25 -0700

    Switching to wdl_path.name

--- tests/cromwellapi/test-validate.py
+++ tests/cromwellapi/test-validate.py
@@ -11,8 +11,7 @@ wdl_paths = list(root.glob(pattern))
 def test_validate_good_wdl(cromwell_api):
     """Checking that validate works - final state is quick"""
     for wdl_path in wdl_paths:
-        wdl_name = str(wdl_path).split("/")[-1]
-        if not wdl_name.startswith("badVal"):
+        if not wdl_path.name.startswith("badVal"):
             res = cromwell_api.validate(wdl_path=wdl_path)
             assert isinstance(res, dict)
             assert res["valid"]
@@ -25,11 +24,10 @@ def test_validate_bad_wdl(cromwell_api):
     """Checking that validate works - final state is quick"""
     message_check = {"badValMissingValue.wdl": "Cannot lookup value 'docker_image'"}
     for wdl_path in wdl_paths:
-        wdl_name = str(wdl_path).split("/")[-1]
-        if wdl_name.startswith("badVal"):
+        if wdl_path.name.startswith("badVal"):
             res = cromwell_api.validate(wdl_path=wdl_path)
             assert isinstance(res, dict)
             assert not res["valid"]
             assert not res["validWorkflow"]
             assert not res["isRunnableWorkflow"]
-            assert message_check[wdl_name] in res["errors"][0]
+            assert message_check[wdl_path.name] in res["errors"][0]
