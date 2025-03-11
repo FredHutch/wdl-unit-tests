73b8746c14e70207f5f4b44446354e5955b0735b
tefirman <tefirman@gmail.com>
2025-03-11 15:07:23 -0700

    Adding docstrings to functions and comments to assertions

--- tests/cromwellapi/test-successes.py
+++ tests/cromwellapi/test-successes.py
@@ -14,7 +14,14 @@ params = {
 
 @pytest.mark.vcr
 def test_successes_initial(cromwell_api, submit_wdls, recording_mode):
-    """Checking for failures works for initial state"""
+    """
+    Parametrized pytest used to ensure that each WDL unit test got submitted successfully
+    with the exception of WDL's that are expected to fail (name starts with "bad")
+
+    Args:
+        cromwell_api (CromwellApi): PROOF server being used to submit WDL unit tests (class defined in cromwell.py)
+        wdl_path (PosixPath): location of the WDL script to submit via PROOF
+    """
     succeed = list(filter(lambda x: not x["path"].startswith("bad"), submit_wdls))
     for job in succeed:
         if recording_mode != "rewrite":
@@ -35,12 +42,21 @@ def test_successes_initial(cromwell_api, submit_wdls, recording_mode):
                         "id",
                     ]
                 )
+                # Checking to make sure the workflow hasn't started yet
+                # This dictionary represents executions of the individual tasks of the WDL workflow
                 assert res["calls"] == {}
 
 
 @pytest.mark.vcr
 def test_successes_final(cromwell_api_final, submit_wdls):
-    """Checking for failures works for final state"""
+    """
+    Parametrized pytest used to ensure that each WDL unit test executes successfully
+    with the exception of WDL's that are expected to fail (name starts with "bad")
+
+    Args:
+        cromwell_api (CromwellApi): PROOF server being used to execute WDL unit tests (class defined in cromwell.py)
+        wdl_path (PosixPath): location of the WDL script to execute via PROOF
+    """
     succeed = list(filter(lambda x: not x["path"].startswith("bad"), submit_wdls))
     for job in succeed:
         res = cromwell_api_final.metadata(workflow_id=job["id"], params=params)
@@ -52,5 +68,7 @@ def test_successes_final(cromwell_api_final, submit_wdls):
                 "id",
             ]
         )
+        # Checking to make sure the workflow was actually executed (and not pulled from the cache)
+        # If a previous execution is pulled from the cache, this dictionary would be empty
         assert len(res["calls"]) > 0
         assert res["status"] == "Succeeded"
