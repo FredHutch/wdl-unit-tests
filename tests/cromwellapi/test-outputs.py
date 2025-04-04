4b5c39782232e80553116ef365b15d86a5d45743
tefirman <tefirman@gmail.com>
2025-04-04 14:41:22 -0700

    Adding input arguments to docstrings

--- tests/cromwellapi/test-outputs.py
+++ tests/cromwellapi/test-outputs.py
@@ -7,6 +7,10 @@ def test_outputs(cromwell_api, submit_wdls):
     Getting workflow outputs works
 
     Cromwell outputs route (/api/workflows/v1/{workflow_id}/outputs)
+
+    Args:
+        cromwell_api (CromwellApi): PROOF server being used to submit WDL unit tests (class defined in cromwell.py)
+        recording_mode (str): string indicating if the cassettes are getting rewritten or not
     """
     ids = [wf["id"] for wf in submit_wdls]
     for x in ids:
