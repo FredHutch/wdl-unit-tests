08f6f2698ba3405214fe8559034401c315e4091a
Scott Chamberlain <sachamber@fredhutch.org>
2025-05-19 12:12:31 -0700

    show rich formatting, add check for data dirs

--- tests/validate_wdls.py
+++ tests/validate_wdls.py
@@ -2,11 +2,12 @@ import logging
 
 from cromwellapi.utils import fetch_wdl_paths
 from rich.logging import RichHandler
+from rich.text import Text
 
 logging.basicConfig(
     level=logging.WARNING,
     format="%(message)s",
-    handlers=[RichHandler(show_time=False)],
+    handlers=[RichHandler(show_time=False, markup=True)],
 )
 log = logging.getLogger("rich")
 
@@ -14,12 +15,14 @@ log = logging.getLogger("rich")
 def validate_wdls(wdl_paths):
     [check_wdl_name_matches(w) for w in wdl_paths]
     [check_for_other_json_files(w) for w in wdl_paths]
+    [check_data_dirs(w) for w in wdl_paths]
 
 
 def check_wdl_name_matches(path):
     if not path.name.replace(".wdl", "") == path.parent.name:
         log.warning(
-            f"WDL file name {path.name} and dir name {path.parent.name} do not match"
+            f"WDL dir name [u]{path.parent.name}[/] and file name"
+            f" [b yellow]{path.name}[/] do not match"
         )
 
 
@@ -29,7 +32,20 @@ def check_for_other_json_files(path):
         for json in jsons:
             if json.name not in ["options.json", "inputs.json"]:
                 log.warning(
-                    f"WDL dir {json.parent.name} has additional json files beyond inputs/options"
+                    f"WDL dir [u]{json.parent.name}[/] has additional json"
+                    f" files beyond inputs/options: [b yellow]{json.name}[/]"
+                )
+
+
+def check_data_dirs(path):
+    dirs = [x for x in path.parent.iterdir() if x.is_dir()]
+    if dirs:
+        for ele in dirs:
+            if ele.name not in ["data"]:
+                log.warning(
+                    f"WDL dir [u]{path.parent.name}[/] has subdir "
+                    f"[b yellow]{ele.name}[/]; only dir allowed is "
+                    f"[b green]data[/]"
                 )
 
 
