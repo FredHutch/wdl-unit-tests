ba64cccd93421f7f671578f8ae18907cd53a42cb
Scott Chamberlain <sachamber@fredhutch.org>
2025-07-16 11:12:46 -0700

    fix docstring

--- tests/cromwellapi/test-regulated_paths.py
+++ tests/cromwellapi/test-regulated_paths.py
@@ -7,12 +7,11 @@ import pytest
 @pytest.mark.vcr
 def test_regulated_paths(proof_api):
     """
-    Test the Cromwell version endpoint (/engine/v1/version)
-
-    We're not testing exact version number as we just care about the data type
+    Test that the scratch directory is within the regulated data path
+    when using a regulated data Cromwell server
 
     Args:
-        cromwell_api (CromwellApi): Cromwell server being used to submit WDL unit tests (class defined in cromwell.py)
+        proof_api (ProofApi): ProofApi instance from proof.py
     """
     if not hasattr(proof_api, "regulated_data"):
         pytest.skip("Regulated data not enabled")
